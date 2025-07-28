9c94b40bdee13161d4adbffc53cf660077697453
Josafá Souza Jr. <josafassj@gmail.com>
2025-07-28 11:21:50 -0300

    Update user-data script to configure iptables for HTTP and HTTPS access

--- user-data-ubuntu.sh
+++ user-data-ubuntu.sh
@@ -4,9 +4,11 @@ apt update -y
 apt install -y python3-pip python3-venv python3-dev python3 \
     build-essential libssl-dev libffi-dev python3-setuptools nginx git
 
-
-# /etc/iptables/rules.v4
-# iptables-restore < /etc/iptables/rules.v4
+cat >> /etc/iptables/rules.v4 << EOL
+-A INPUT -p tcp -m state --state NEW -m tcp --dport 80 -j ACCEPT
+-A INPUT -p tcp -m state --state NEW -m tcp --dport 443 -j ACCEPT
+EOL
+iptables-restore < /etc/iptables/rules.v4
 
 # iptables -I INPUT 6 -m state --state NEW -p tcp --dport 80 -j ACCEPT
 # netfilter-persistent save    
